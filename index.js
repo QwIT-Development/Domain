b8fcdeec1a84ce3c0997ec43f24c1638136f59fd
BalazsManus <contact@balazsmanus.hu>
2025-03-14 20:32:58 +0100

    added some initializers to the code

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // async main thread hell yeah
 async function main() {
+    // imports
+    const {Events} = require("discord.js");
+    const promptLoader = require('./initializers/geminiClient');
+
     // initialize stuff inside async thingy
-    const client = require('./initializers/botClient');
+    let discordClientReady = false;
+    const discordClient = require('./initializers/botClient');
+    discordClient.once(Events.ClientReady, () => {
+        discordClientReady = true;
+    });
+    while (!discordClientReady) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+    }
+
+    const geminiSession = promptLoader();
+
+
 
 }
 
-main();
+main().then();
