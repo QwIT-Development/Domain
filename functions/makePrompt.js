9173d76abf2b56b70e1507ba761ea65083a25888
BalazsManus <contact@balazsmanus.hu>
2025-03-14 20:33:16 +0100

    a bit different prompt loading mechanism

---
+++ functions/makePrompt.js
@@ -0,0 +1,5 @@
+async function makePrompt() {
+
+}
+
+module.exports = makePrompt;
--- initializers/geminiClient.js
+++ initializers/geminiClient.js
@@ -1,23 +1,26 @@
 const {GoogleGenerativeAI} = require("@google/generative-ai");
-
 const config = require('../config.json');
 const genAI = new GoogleGenerativeAI(config.GEMINI_API_KEY);
+const makePrompt = require('../functions/makePrompt');
 
-const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash-exp",
-});
 const generationConfig = {
     temperature: 1.15,
     topP: 0.95,
-    topK: 64,
+    topK: 40,
     maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
+    responseMimeType: 'text/plain'
 };
 
+async function promptLoader() {
+    const model = genAI.getGenerativeModel({
+        model: "gemini-2.0-flash-exp",
+        systemInstruction: await makePrompt()
+    });
 
-const chatSession = model.startChat({
-    generationConfig,
-    history: [],
-});
+    return model.startChat({
+        generationConfig,
+        history: [],
+    });
+}
 
-module.exports = chatSession;
+module.exports = promptLoader;
