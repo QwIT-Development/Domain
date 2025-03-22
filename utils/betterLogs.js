588a550fc58f46eb2246762662ea5aa859292c15
balazsmanus <contact@mnus.moe>
2025-03-22 16:30:48 +0100

    add license headers

--- utils/betterLogs.js
+++ utils/betterLogs.js
@@ -1,3 +1,9 @@
+/*
+        Domain-Unchained, src of the discord bot, that uses gemini api to generate messages
+        Copyright (C) 2025  BalazsManus
+*/
+
+
 function log(message, type = "info", thread = "index.js") {
     // https://colors.sh/
     const colors = {
--- utils/initData.js
+++ utils/initData.js
@@ -1,3 +1,9 @@
+/*
+        Domain-Unchained, src of the discord bot, that uses gemini api to generate messages
+        Copyright (C) 2025  BalazsManus
+*/
+
+
 const fs = require('fs');
 const path = require('path');
 
