e6a2f366f0606caa6ea0ad5170ed2958fb394dab
balazsmanus <contact@mnus.moe>
2025-03-23 13:51:09 +0100

    working fuzzy search

--- utils/fuzzySearch.js
+++ utils/fuzzySearch.js
@@ -1,24 +1,26 @@
 const Fuse = require('fuse.js');
 const log = require('../utils/betterLogs');
 
-function splitFuzzySearch(pattern, strings, options = {}) {
+function splitFuzzySearch(pattern, strings, options = {"includeScore": true}) {
     if (!pattern || !strings || strings.length === 0) {
         return false;
     }
 
-    const words = pattern.split(/\s+/);
-    const wordMatches = [];
+    // split pharse into words, yes it works with dévid
+    const words = pattern.match(/\S+/g);
+    let allWordsMatch = false;
 
     for (const word of words) {
-        log(`Checking word: ${word}`, 'info', 'fuzzySearch.js');
         const fuse = new Fuse(strings, options);
         const results = fuse.search(word);
-        log(`Results: ${JSON.stringify(results)}`, 'info', 'fuzzySearch.js');
 
-        wordMatches.push(results.length > 0 && results[0].score <= 0.2);
+        if (results.length > 0 && results[0].score <= 0.3) {
+            allWordsMatch = true;
+            break;
+        }
     }
 
-    return wordMatches.every(match => match);
+    return allWordsMatch;
 }
 
 function fuzzySearch(pattern, strings, options = {}) {
