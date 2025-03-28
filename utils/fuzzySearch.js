eb7fadabc9a66aaa5e77abb14ae664f75e1a838c
balazsmanus <contact@mnus.moe>
2025-03-28 20:26:20 +0100

    lemaradt

--- utils/fuzzySearch.js
+++ utils/fuzzySearch.js
@@ -1,3 +1,8 @@
+/*
+        Domain-Unchained, src of the discord bot, that uses gemini api to generate messages
+        Copyright (C) 2025  BalazsManus
+*/
+
 const Fuse = require('fuse.js');
 const log = require('../utils/betterLogs');
 
