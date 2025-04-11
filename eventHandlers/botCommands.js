c229bd06dcfa210a05a595b5d9c2b677ec48e222
balazsmanus <contact@mnus.moe>
2025-04-11 11:17:43 +0200

    add mute command

--- eventHandlers/botCommands.js
+++ eventHandlers/botCommands.js
@@ -6,6 +6,10 @@
 
 // TODO: finish ts
 const state = require("../initializers/state");
+const log = require("../utils/betterLogs");
+const config = require("../config.json");
+const searchHandler = require("./searchHandler");
+const {reputation} = require("../utils/reputation");
 
 async function parseBotCommands(string, message, gemini) {
     let out = string;
@@ -13,15 +17,19 @@ async function parseBotCommands(string, message, gemini) {
     // rep handling
     const {reputation} = require("../utils/reputation");
     if (out.includes("[+rep]" || out.includes("[-rep]"))) {
-        const user = message.author.id
-        if (out.includes("[+rep]")) {
-            await reputation(user, "increase");
-            out = out.replaceAll("[+rep]", "");
-            await message.react(state.emojis["upvote"]);
-        } else if (out.includes("[-rep]")) {
-            await reputation(user, "decrease");
-            out = out.replaceAll("[-rep]", "");
-            await message.react(state.emojis["downvote"]);
+        try {
+            const user = message.author.id
+            if (out.includes("[+rep]")) {
+                await reputation(user, "increase");
+                out = out.replaceAll("[+rep]", "");
+                await message.react(state.emojis["upvote"]);
+            } else if (out.includes("[-rep]")) {
+                await reputation(user, "decrease");
+                out = out.replaceAll("[-rep]", "");
+                await message.react(state.emojis["downvote"]);
+            }
+        } catch (e) {
+            log(`Failed to execute reputation command: ${e}`, 'error', 'botCommands.js');
         }
     }
 
@@ -33,18 +41,47 @@ async function parseBotCommands(string, message, gemini) {
     // handle search
     const searchHandler = require("./searchHandler");
     if (out.includes("search[")) {
-        // example: search[minceraft r34]
-        // ^ ez a komment a regi kodbol jon, lehet zypherift irta de nem biztos
-        const regex = /search\[(.+?)]/gmi;
-        const match = regex.exec(out);
-        if (match) {
-            out = await searchHandler(match[1], message.channel.id, gemini);
-            await message.react(state.emojis["search"]);
+        try {
+            // example: search[minceraft r34]
+            // ^ ez a komment a regi kodbol jon, lehet zypherift irta de nem biztos
+            const regex = /search\[(.+?)]/gmi;
+            const match = regex.exec(out);
+            if (match) {
+                out = await searchHandler(match[1], message.channel.id, gemini);
+                await message.react(state.emojis["search"]);
+            }
+        } catch (e) {
+            log(`Failed to execute search command: ${e}`, 'error', 'botCommands.js');
         }
     }
 
     if (out.includes("mute[")) {
-
+        try {
+            // example: mute[user_id,time,reason]
+            const regex = /mute\[(\S+), ?(\S+), ?(\S+)]/gmi;
+            const match = regex.exec(out);
+            if (match) {
+                const userId = match[1];
+                if (userId !== message.author.id && !config.OWNERS.includes(message.author.id)) {
+                    out = out.replaceAll(match[0], '[Nem némíthatsz el mást]').trim();
+                }
+                const time = parseInt(match[2]) * 1000;
+                const reason = match[3];
+                const guild = message.guild;
+                out = out.replaceAll(match[0], "").trim();
+                if (guild) {
+                    try {
+                        const member = await guild.members.fetch(userId);
+                        await member.timeout(time, reason);
+                        await message.react(state.emojis["mute"]);
+                    } catch (e) {
+                        log(`Failed to mute user: ${e}`, 'error', 'botCommands.js');
+                    }
+                }
+            }
+        } catch (e) {
+            log(`Failed to execute mute command: ${e}`, 'error', 'botCommands.js');
+        }
     }
 
     return out;
