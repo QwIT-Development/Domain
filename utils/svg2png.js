d548c548b43d2c9fdc62a21e58187911547baa9e
balazsmanus <contact@mnus.moe>
2025-04-17 11:33:57 +0200

    fix svg generator

--- utils/svg2png.js
+++ utils/svg2png.js
@@ -3,15 +3,19 @@ const { Readable } = require('stream');
 
 const svgToPng = async (svgCode) => {
     // get resolution from code via the best thing, regex!!1!
-    const viewBoxRegex = /width="([^"]+)" ?height="([^"]+)"/gmi;
+    const viewBoxRegex = /width="([^"]+)" ?height="([^"]+)"/mi;
     const viewBoxMatch = svgCode.match(viewBoxRegex);
 
     let width = 500;
     let height = 500;
     if (viewBoxMatch) {
-        width = parseInt(viewBox[1], 10);
-        height = parseInt(viewBox[2], 10);
+        width = parseInt(viewBoxMatch[1], 10);
+        height = parseInt(viewBoxMatch[2], 10);
     }
+
+    // if the code above fails (90% esely), then fallback to default 500x500
+    if (isNaN(width)) width = 500;
+    if (isNaN(height)) height = 500;
 
     const pngBuffer = await sharp(Buffer.from(svgCode))
         .resize(width, height)
