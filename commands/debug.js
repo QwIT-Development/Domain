157aa74a7c94de9d2aa3825cddd346635f2046df
balazsmanus <contact@mnus.moe>
2025-06-25 11:09:22 +0200

    sum debug commands

---
+++ commands/debug.js
@@ -0,0 +1,64 @@
+/*
+        Domain-Unchained, src of the discord bot, that uses gemini api to generate messages
+        Copyright (C) 2025 Anchietae
+*/
+
+const { SlashCommandBuilder } = require('discord.js');
+const { loadConfig } = require('../initializers/configuration');
+const config = loadConfig();
+const client = global.discordClient;
+const state = require('../initializers/state');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('debug')
+        .setDescription('a few debug functions')
+        .addStringOption(option =>
+            option.setName('subcommand')
+                .setDescription('The subcommand to execute')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'show servers', value: 'show_servers' },
+                    { name: 'show history count for channel', value: 'channel_history_count' }
+                )
+        ),
+
+    async execute(interaction) {
+        if (interaction.user.id !== '710839743222513715') {
+            await interaction.reply({
+                content: 'You are not allowed to use this command.',
+                flags: ['Ephemeral']
+            });
+            return;
+        }
+
+        const subcommand = interaction.options.getString('subcommand');
+
+        if (subcommand === 'show_servers') {
+            let response = "The server the bot present in:\n";
+            client.guilds.cache.forEach(guild => {
+                const channels = guild.channels.cache.map(channel => channel.id);
+                let configChannelIds = Object.keys(config.CHANNELS);
+                const guildShouldBeLeft = !channels.some(channelId => configChannelIds.includes(channelId));
+
+                if (guildShouldBeLeft) {
+                    response += `${guild.name} (${guild.id}) - Unknown guild.\n`;
+                } else {
+                    response += `${guild.name} (${guild.id}) - Known guild.\n`;
+                }
+            response += `-# This information might not be up to date.`;
+            });
+        }
+
+        if (subcommand === 'channel_history_count') {
+            const channel = interaction.channel.id;
+            const historyCount = state.history[channel].length || 0;
+            await interaction.reply({
+                content: `This channel has ${historyCount} messages in history.`,
+                flags: ['Ephemeral']
+            });
+            return;
+        }
+    }
+
+};
